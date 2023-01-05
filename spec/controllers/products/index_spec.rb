7aae4b732bc86ab8393db272126328d13419bb4d
codescaptain <codescaptain@gmail.com>
2023-01-05 15:25:36 +0300

    add ProductController#index test

--- spec/controllers/products/index_spec.rb
+++ spec/controllers/products/index_spec.rb
@@ -5,10 +5,16 @@ RSpec.describe ProductsController, type: :controller do
 
   describe "GET index" do
     let!(:products) { create_list(:product, 5) }
-    it "should return all products" do
+    before(:each) do
       get :index, format: :json
-      products.map { |product| product.id}
+    end
+
+    it "should return all products" do
       expect(parsed_body.pluck("id")).to eq(products.map { |product| product.id})
+    end
+
+    it 'should return 200 HTTP status' do
+      expect(response.status).to eq(200)
     end
   end
 end
