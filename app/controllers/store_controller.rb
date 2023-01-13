1f555de1f546de57d32d88317c364dbbc021bcea
codescaptain <codescaptain@gmail.com>
2023-01-13 04:36:16 +0300

    add LineItems model

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,7 @@
 class StoreController < ApplicationController
+  include VisitedCount
+
+  before_action :total_visited_count, only: %i[ index ]
   def index
     @products = Product.order(:title)
   end
